66ad381caa9cc4e266437cd15a420cbb25ecf5c7
mikepadiernos <mike.padiernos@gmail.com>
2020-08-20 22:40:57 -0400

    [BUILD] setup / component files
    - started base code for header, main, & controls

---
+++ src/components/Controls/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Controls() {
+	return (
+		<div>
+
+		</div>
+	)
+
+}
+
+export default Controls;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from "../../assets/images/logo.svg";
+
+function Header() {
+	return (
+		<header className="App-header">
+			<h1 className="title no-top">
+				Conway's Game of Life
+			</h1>
+		</header>
+	)
+}
+
+export default Header;
